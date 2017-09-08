1967d4ed77b7f57410c91a1dffd6fe420fa78044
Tuyền Nguyễn <tuyendev@gmail.com>
2017-09-08 15:24:45 +0700

    Create Autoinjector.java

---
+++ Template/src/com/tuyendev/inject/Autoinjector.java
@@ -0,0 +1,14 @@
+package com.tuyendev.inject;		
+	
+import java.lang.annotation.ElementType;		
+import java.lang.annotation.Retention;		
+import java.lang.annotation.RetentionPolicy;		
+import java.lang.annotation.Target;		
+	
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})		
+@Retention(RetentionPolicy.RUNTIME)		
+public @interface Autoinjector {		
+    String mappedName() default "";		
+    String injectType() default InjectorType.REMOTE;		
+    String refName() default "";		
+}
