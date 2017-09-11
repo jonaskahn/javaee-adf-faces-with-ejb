026d0670bf3c92d5b8336cc78a1b6f216dc279d8
Tuyền Nguyễn <tuyendev@gmail.com>
2017-09-11 14:21:22 +0700

    update region form.
    update region form.

---
+++ Template/public_html/resources/js/template.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.dropdown-submenu a.test').on("click", function (e) {
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    $('.state_menu_active').addClass("glyphicon-folder-open");
+    $('.state_menu_').addClass("glyphicon-folder-close");
+    
+});
