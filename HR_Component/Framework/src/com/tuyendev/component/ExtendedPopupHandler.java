8ab1be073647fff3649359e961f787823e55209a
Tuyền Nguyễn <tuyendev@gmail.com>
2017-09-29 22:03:34 +0700

    working with popup tag

---
+++ HR_Component/Framework/src/com/tuyendev/component/ExtendedPopupHandler.java
@@ -0,0 +1,14 @@
+package com.tuyendev.component;
+
+import com.tuyendev.controller.BaseController;
+
+import javax.faces.event.ActionEvent;
+
+public class ExtendedPopupHandler extends BaseController {
+    
+
+    public String closePopup() {
+        // Add event code here...
+        return null;
+    }
+}
